a65b47ed0d8bc3e718b75c13ba8812ea940ec453
disha2027 <dishabhat47@gmail.com>
2025-04-18 20:34:56 +0530

    Create Hero.jsx file is added

---
+++ src/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section className='hero'>
+    <div className='content'>
+      <div className='title'>
+        <h1>GET</h1>
+        <h1>FIT</h1>
+        <h1>WITH US</h1>
+        <div className='sub-title'>
+          <p>Your Journey to Fitness Starts Here</p>
+          <p>Build Your Body Strong</p>
+        </div>
+          <button>Start Your Journey</button>
+          <button>Discover Your Plan</button>
+      </div>
+    </div>
+
+   </section>
+  )
+}
+
+export default Hero
